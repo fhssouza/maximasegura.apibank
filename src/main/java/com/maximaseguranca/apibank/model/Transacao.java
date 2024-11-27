5919caec00f1eb99f2a4ffcac4e4b21242771097
fhssouza <fhssouza@gmail.com>
2024-11-27 17:23:58 -0300

    Models Transação e Usuario

---
+++ src/main/java/com/maximaseguranca/apibank/model/Transacao.java
@@ -0,0 +1,70 @@
+package com.maximaseguranca.apibank.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transacoes")
+public class Transacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String contaOrigem;
+    private String contaDestino;
+    private BigDecimal valor;
+    private LocalDateTime dataTransacao;
+
+    public Transacao(String contaOrigem, String contaDestino, BigDecimal valor) {
+        this.contaOrigem = contaOrigem;
+        this.contaDestino = contaDestino;
+        this.valor = valor;
+        this.dataTransacao = LocalDateTime.now();
+    }
+
+    public Transacao() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContaOrigem() {
+        return contaOrigem;
+    }
+
+    public void setContaOrigem(String contaOrigem) {
+        this.contaOrigem = contaOrigem;
+    }
+
+    public String getContaDestino() {
+        return contaDestino;
+    }
+
+    public void setContaDestino(String contaDestino) {
+        this.contaDestino = contaDestino;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public LocalDateTime getDataTransacao() {
+        return dataTransacao;
+    }
+
+    public void setDataTransacao(LocalDateTime dataTransacao) {
+        this.dataTransacao = dataTransacao;
+    }
+}
---
+++ src/main/java/com/maximaseguranca/apibank/model/Usuario.java
@@ -0,0 +1,78 @@
+package com.maximaseguranca.apibank.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private int idade; private String cpf;
+    private String numeroConta;
+    private BigDecimal saldo;
+
+    public Usuario(String nome, int idade, String cpf, String numeroConta) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+        this.numeroConta = numeroConta;
+        this.saldo = BigDecimal.ZERO;
+    }
+
+    public Usuario() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(String numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+}
